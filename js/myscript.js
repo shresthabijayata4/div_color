63b1629918d516eb71f707d064c47fd4857265de
shresthabijayata4 <bijju.stha4@gmail.com>
2021-03-24 18:50:19 +0545

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,227 @@
+
+    // var div = document.createElement('div');
+    
+    // div.id = 'container';
+    // div.innerHTML = 'Hi there!';
+    // div.className = 'border pad';
+   
+    // document.body.appendChild(div);
+
+    for(x=0; x<3;x++) {
+        var div_row = document.createElement('div');
+        div_row.className = 'row_container';
+        div_row.setAttribute(
+            "style", "border: 2px solid black; height: 200px; padding:30px; display: flex; margin: 10px 0px");
+       document.body.appendChild(div_row);
+        for(y=0; y<3;y++) {
+            var div_col = document.createElement('div');
+            div_col.className='col_container';
+            div_col.setAttribute(
+                "style", "border: 1px solid black; height: 100px; width:100%; padding:30px; margin: 0px 20px"
+                );
+       
+        div_row.appendChild(div_col);
+       
+        }      
+    }
+
+  
+  
+    // div_row.appendChild(div_col);
+    // div_row.appendChild(div_col);
+
+  
+
+   
+
+    
+
+
+
+
+
+
+
+
+
+
+// const row1 = document.createElement("div");
+// const row2 = document.createElement('div');
+// const row3 = document.createElement('div');
+// const col1=document.createElement('div');
+// const col2=document.createElement('div');
+// const col3=document.createElement('div');
+// const col4=document.createElement('div');
+// const col5=document.createElement('div');
+// const col6=document.createElement('div');
+// const col7=document.createElement('div');
+// const col8=document.createElement('div');
+// const col9=document.createElement('div');
+// const h1 = document.createElement("h1");
+// const node = document.createTextNode("Welcome!!");
+
+// row1.id ="row_first";
+// row1.setAttribute("style", "border: 2px solid black");
+// row2.id="row_second";
+// row3.id="row_third";
+// col1.id="col_first";
+// col2.id="col_second";
+// col3.id="col_third";
+// col4.id="col_fourth";
+// col5.id="col_fifth";
+// col6.id="col_sixth";
+// col7.id="col_seventh";
+// col8.id="col_eighth";
+// col9.id="col_ninth";
+
+// h1.appendChild(node);
+// var body = document.getElementsByTagName("body")[0];
+
+// body.appendChild(row1);
+// const element = document.getElementById("row_first");
+// element.appendChild(h1);
+
+
+// var para = document.createElement("p");
+// var h1 = document.createElement("h1");
+// var div = document.createElement('div');
+// var node = document.createTextNode("Welcome!!")
+// var btn = document.createElement("BUTTON");
+// const row1 = document.createElement("div");
+// row1.id ="row_first";
+
+
+// h1.appendChild(node);
+
+// document.body.appendChild(div);
+// var element = document.getElementById("row_first");
+
+// element.appendChild(h1);
+
+// var button = document.createElement("button");
+// button.innerHTML = "Switch color";
+
+// var body = document.getElementsByTagName("body")[0];
+// body.appendChild(button);
+
+// document.querySelector("body").style.textAlign = "center";
+
+// function randmColor() {
+//     let color = "#";
+//     for (let i = 0; i < 6; i++) {
+//         color += Math.floor((Math.random() * 16)).toString(16);
+//     }
+
+//     return color;
+// }
+
+
+// document.querySelector("button").addEventListener("click", function () {
+   
+//     document.querySelector("body").style.background = randmColor();
+  
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// =====================Colors====================
+
+// var color =["#35FF69", "#44CCFF","#7494EA", "#D138BF", "#494947", "#3DE6B4", "#f20505", "#f0e80a"];
+// var i=0;
+// const rannumber1=Math.floor(Math.random() * 256 );
+// const rannumber2=Math.floor(Math.random() * 256 );
+// const rannumber3=Math.floor(Math.random() * 256 );
+// const ranrgb="rgb("+rannumber1+","+rannumber2+","+rannumber3+")";
+
+
+
+// const random =Math . floor(Math.random() * months.length);
+
+// button.addEventListener("click", function() {
+
+//     alert("random, months[random]");
+// })
+
+
+
+// var para = document.createElement("p");
+// var node = document.createTextNode("This is new.");
+// para.appendChild(node);
+// var element = document.getElementById("div1");
+
+
+// element.appendChild(para);
+// console.log(element);
+
+
+
+// ===========================other processes====================================
+
+
+
+// =====================Colors====================
+
+// var color =["#35FF69", "#44CCFF","#7494EA", "#D138BF", "#494947", "#3DE6B4", "#f20505", "#f0e80a"];
+// var i=0;
+// const rannumber1=Math.floor(Math.random() * 256 );
+// const rannumber2=Math.floor(Math.random() * 256 );
+// const rannumber3=Math.floor(Math.random() * 256 );
+// const ranrgb="rgb("+rannumber1+","+rannumber2+","+rannumber3+")";
+
+
+
+// function randmColor() {
+//     let color = "#";
+//   for(let i = 0; i < 6; i++){
+//         color += Math.floor((Math.random() * 16)).toString(16);
+//   }
+//     return color;
+
+// }
+
+
+// document.querySelector("button").addEventListener("click", function(){
+//     // i = 1<color.length? ++i : 0;
+//     document.querySelector("body").style.background=ranrgb;
+// document.querySelector("body").style.background=color[i]
+
+
+// });
+
+
+
+
+// const random =Math . floor(Math.random() * months.length);
+
+// button.addEventListener("click", function() {
+
+//     alert("random, months[random]");
+// })
+
+
+
+// var para = document.createElement("p");
+// var node = document.createTextNode("This is new.");
+// para.appendChild(node);
+// var element = document.getElementById("div1");
+
+
+// element.appendChild(para);
+// console.log(element);
+
+
+
+
+
+
+
